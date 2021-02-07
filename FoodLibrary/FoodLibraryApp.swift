995c659f78f4693fdf6b611a57f744c37bcfb984
Elise <erbuzzell@gmail.com>
2021-02-07 10:03:57 -0600

    Initial Commit

---
+++ FoodLibrary/FoodLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodLibraryApp.swift
+//  FoodLibrary
+//
+//  Created by Elise Buzzell on 2/7/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
