65f8393457d4b2bd765b6442f95b76c15ca2c75a
Elise <erbuzzell@gmail.com>
2021-02-07 14:47:48 -0600

    added storyboard, created the basic UI, need to start connecting UI to code but unable to find @class ViewContoller: UIViewController {

--- FoodLibrary/ContentView.swift
+++ FoodLibrary/ContentView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack {
+                VStack {
+                    Label(/*@START_MENU_TOKEN@*/"Meal Name"/*@END_MENU_TOKEN@*/, systemImage: /*@START_MENU_TOKEN@*/""/*@END_MENU_TOKEN@*/)
+                        .padding(0.0)
+                    VStack {
+                        TextField("Enter Meal Name"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(""))
+                            .alignmentGuide(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=Guide@*/.top/*@END_MENU_TOKEN@*/) { dimension in
+                                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/dimension[.top]/*@END_MENU_TOKEN@*/
+                            }
+                        Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
+                        }
+                        
+                    }
+                }
+                            
+            }
+    
+            
+        }
     }
 }
 
@@ -19,3 +38,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
